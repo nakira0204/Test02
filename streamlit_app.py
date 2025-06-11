5c7cb0a5fe5ad3a83ef2d226b6ed0fcc60cf20fa
nakira0204 <nakira@wb4.so-net.ne.jp>
2025-06-11 10:15:56 +0000

    SImplify the code

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+
+# Write directly to the app
+st.title(f"Example Streamlit App :balloon: {st.__version__}")

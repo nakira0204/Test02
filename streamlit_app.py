da0a18b59ffc837de3d4cec88ae56f5aa1bbb85e
nakira0204 <nakira@wb4.so-net.ne.jp>
2025-06-16 12:02:28 +0000

    GIT 04 Revised 02

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
-st.title(f"GIT04 Revised 01 :balloon: {st.__version__}")
+st.title(f"GIT04 Revised 02:balloon: {st.__version__}")
